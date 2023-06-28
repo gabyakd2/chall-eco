25d447318734b79e858fc4d85ac268ded47c5490
gabyakd2 <gabrielsal00@hotmail.com>
2023-06-28 12:16:07 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        appDir: true
+    },
+    // env: {
+    //     API_URL: ""
+    // }
+}
 
 module.exports = nextConfig
